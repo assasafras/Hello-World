330c3b2512ae721d47357a29fce98c804fd64995
assasafras <isaac.joseph.brown@gmail.com>
2017-08-07 14:21:00 +1200

    Added unity3d project
    Up to video 5 of creative coding thing.

---
+++ Assets/Scripts/Rotate.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour
+{
+  public Vector3 Rotation;
+
+  public bool Orbit;
+
+  public Vector3 OrbitAxis;
+
+  public GameObject ObjectToOrbit;
+
+  public float OrbitSpeed;
+
+  // Use this for initialization
+  void Start()
+  {
+
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+    if (Orbit)
+      transform.RotateAround(ObjectToOrbit.transform.position, OrbitAxis, OrbitSpeed * Time.deltaTime);
+    else
+      transform.Rotate(Rotation * Time.deltaTime);
+  }
+}
